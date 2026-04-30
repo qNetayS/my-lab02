2985b49b4af7f79eebbbd32288a4ddb35760af3c
qNetayS <petrenk007@yandex.ru>
2026-04-30 20:33:08 +0300

    add comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-#include <string>
+#include<isotream>
+#include<string>
+//main func
 int main(){
     std::string name;
     std::cout << "your name";
-    std::cin >> name;
-    std::cout << "Hello world from" << name << std::endl;
+    std::cin >> name: //you can give youe name
+    //output name
+    std::cout << "hello" << name << std::endl;
     return 0;
 }
